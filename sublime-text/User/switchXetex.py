f6fb9143b1866bee814a1336367afd49d0b5d01b
Victor Cuadrado Juan <victorcuad@gmail.com>
2013-02-07 02:23:19 +0100

    updated switchXetex.py to get home path variable

--- sublime-text/User/switchXetex.py
+++ sublime-text/User/switchXetex.py
@@ -2,11 +2,13 @@ import sublime
 import sublime_plugin
 import shutil
 import filecmp
+import os
 
 
 class SwitchCommand(sublime_plugin.ApplicationCommand):
     def run(self):
-        folder = '/home/vic/.config/sublime-text-2/Packages/User/'
+        home = os.path.expanduser('~')  # extract home path (works in unix and windows)
+        folder = home + '/.config/sublime-text-2/Packages/User/'
         latex_src = folder + 'LaTeX.sublime-build.latex'
         xetex_src = folder + 'LaTeX.sublime-build.xetex'
         dest = folder + 'LaTeX.sublime-build'
@@ -16,3 +18,4 @@ class SwitchCommand(sublime_plugin.ApplicationCommand):
         elif filecmp.cmp(dest, xetex_src):
             sublime.status_message("Switching to LaTeX")
             shutil.copy(latex_src, dest)
+
